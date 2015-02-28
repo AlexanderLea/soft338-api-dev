6fd66ea3d0cbbfdbed1273b99610d8694cf449e8
Alexander <alexander.lea@gmail.com>
2015-02-28 15:41:29 +0000

    Deploy

---
+++ DEPLOY/dbo.Table.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [JobTitle] VARCHAR(150) NOT NULL, 
+    [CompanyName] VARCHAR(150) NOT NULL, 
+    [JobDescription] VARCHAR(500) NULL, 
+    [BusinessSector] VARCHAR(150) NULL, 
+    [Postcode] VARCHAR(10) NOT NULL, 
+    [Town] VARCHAR(50) NULL, 
+    [County] VARCHAR(50) NULL, 
+    [RecruiterName] VARCHAR(50) NULL, 
+    [RecruiterNumber] VARCHAR(50) NULL, 
+    [RecruiterEmail] VARCHAR(50) NULL, 
+    [ApplicationNotes] VARCHAR(500) NULL 
+)
